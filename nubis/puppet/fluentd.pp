434c17d700409aa9ff0463a388c328bc263af152
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-05-28 12:37:25 -0400

    add Ceph specific fluentd collectors

---
+++ nubis/puppet/fluentd.pp
@@ -0,0 +1,29 @@
+class { 'fluentd':
+  service_ensure => stopped
+}
+
+fluentd::configfile { 'ceph': }
+
+fluentd::source { 'ceph-daemons':
+  configfile => 'ceph',
+  type       => 'tail',
+  format     => '/^(?<time>[^ ]* [^ ]*) (?<offset>[^ ]*)[ ]*(?<level>[^ ]*) (?<message>.*)$/',
+  time_format => '%Y-%m-%d %H:%M:%S',
+  tag        => 'forward.ceph.daemons',
+  config     => {
+    'path' => '/var/log/ceph/ceph-**.log',
+  },
+}
+
+fluentd::source { 'ceph-activity':
+  configfile => 'ceph',
+  type       => 'tail',
+  format     => '/^(?<time>[^ ]* [^ ]*) (?<ident>[^ ]*) (?<host>[^ ]*) (?<offset>[^ ]*) : \[(?<level>[^ ]*)\] (?<message>.*)$/',
+  time_format => '%Y-%m-%d %H:%M:%S',
+  tag        => 'forward.ceph.activity',
+  config     => {
+    'path' => '/var/log/ceph/ceph.log',
+  },
+}
+
+
