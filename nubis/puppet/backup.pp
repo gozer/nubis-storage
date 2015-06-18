5f38c6f98d597b8649150c90120b48e9d88d0a62
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-06-18 15:46:22 -0400

    make sure to set PATH correctly for cron jobs

--- nubis/puppet/backup.pp
+++ nubis/puppet/backup.pp
@@ -13,6 +13,6 @@ cron::hourly { 'ceph_backup':
     minute  => '40',
     user    => 'root',
     command => '/usr/local/bin/ceph-s3-backup | tee -a /var/log/ceph-s3-backup.log',
-    environment => "MAILTO=gozer@mozilla.com",
+    environment => "MAILTO=gozer@mozilla.com\nPATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin\n",
     require => File["/usr/local/bin/ceph-s3-backup"],
 }
