a224f2c7cffe3a4fdbeca00bc16622de986a5db7
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-10-04 12:46:13 -0400

    puppet-lint++

--- nubis/puppet/backup.pp
+++ nubis/puppet/backup.pp
@@ -3,16 +3,16 @@ package {'awscli':
 }
 
 file { '/usr/local/bin/ceph-s3-backup':
-  source => "puppet:///nubis/files/ceph-s3-backup",
-  owner => "root",
-  group => "root",
-  mode  => "0755",
+  source => 'puppet:///modules/nubis/files/ceph-s3-backup',
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0755',
 }
 
 cron::hourly { 'ceph_backup':
-    minute  => '40',
-    user    => 'root',
-    command => '/usr/local/bin/ceph-s3-backup | tee -a /var/log/ceph-s3-backup.log',
+    minute      => '40',
+    user        => 'root',
+    command     => '/usr/local/bin/ceph-s3-backup | tee -a /var/log/ceph-s3-backup.log',
     environment => "MAILTO=gozer@mozilla.com\nPATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin\n",
-    require => File["/usr/local/bin/ceph-s3-backup"],
+    require     => File['/usr/local/bin/ceph-s3-backup'],
 }
--- nubis/puppet/datadog.pp
+++ nubis/puppet/datadog.pp
@@ -1,11 +1,11 @@
 # Install DataDog Ceph integration monitor
-notify { "Installing Datadog Ceph integration monitor": }
+notify { 'Installing Datadog Ceph integration monitor': }
 
-file { "/etc/dd-agent/conf.d/ceph.yaml":
-  source => "puppet:///nubis/files/datadog-ceph.yaml",
-  owner => "root",
-  group => "root",
-  mode  => "0644",
+file { '/etc/dd-agent/conf.d/ceph.yaml':
+  source => 'puppet:///modules/nubis/files/datadog-ceph.yaml',
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0644',
 }
 
 
